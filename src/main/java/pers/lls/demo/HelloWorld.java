a4359b5374fc6d422b39cac57d3dcff8d6d2c0b4
lils <2247407869@qq.com>
2020-04-22 10:28:02 +0800

    2

--- src/main/java/pers/lls/demo/HelloWorld.java
+++ src/main/java/pers/lls/demo/HelloWorld.java
@@ -14,13 +14,7 @@ import java.util.stream.Collectors;
  */
 public class HelloWorld {
 
-    private static final String TIME_ERROR_CONTENT = "%s已达到%s（最近%s分钟 %s）,%s阈值%s%s";
-    private static final String TIME_OK_CONTENT = "%s已恢复到%s（最近%s分钟 %s）";
-    private static final String COUNT_ERROR_CONTENT = "%s已达到%s（连续%s次）,%s阈值%s%s";
-    private static final String COUNT_OK_CONTENT = "%s已恢复到%s（连续%s次）";
-    private static final String DYNAMIC_ERROR_CONTENT = "%s已达到%s,连续%s次超过智能分析容忍范围";
-    private static final String DYNAMIC_OK_CONTENT = "%s已恢复到%s，处于智能分析容忍范围内";
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException {
+    public static void main(String[] args) {
 
 
 
