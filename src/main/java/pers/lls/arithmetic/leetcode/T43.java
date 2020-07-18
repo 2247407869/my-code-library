4a98c8b52ce107d2f4d6e8746801a066d58ff28a
lils <lils@broada.com>
2020-07-18 16:17:24 +0800

    leetcode

---
+++ src/main/java/pers/lls/arithmetic/leetcode/T43.java
@@ -0,0 +1,42 @@
+package pers.lls.arithmetic.leetcode;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.StringUtils;
+
+import java.lang.reflect.InvocationTargetException;
+
+
+public class T43 {
+
+//    public String multiply(String num1, String num2) {
+//        int length1 = num1.length();
+//        int length2 = num2.length();
+//        for (int i = 0; i < length1; i++) {
+//            for (int j = 0; j < length2; j++) {
+//
+//            }
+//        }
+//    }
+
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+
+
+        int[] intarray = new int[]{
+                5,1,3
+        };
+
+        int i = 1 ;
+
+        String string ="123";
+
+        String string2 = "456";
+
+        ListNode listNode = new ListNode(1,new ListNode(2,new ListNode(4)));
+
+        ListNode listNode2 = new ListNode(1,new ListNode(3,new ListNode(4)));
+
+//        System.out.println(JSONObject.toJSONString(new T43().multiply(string,string2).equals("56088")));
+    }
+}
+
