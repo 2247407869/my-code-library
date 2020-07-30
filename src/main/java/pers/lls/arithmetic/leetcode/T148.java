a173d374daeee23db937b48e774c0be324948dcd
lils <lils@broada.com>
2020-07-30 13:39:51 +0800

    我再也不想排序链表了

--- src/main/java/pers/lls/arithmetic/leetcode/T148.java
+++ src/main/java/pers/lls/arithmetic/leetcode/T148.java
@@ -8,73 +8,76 @@ import java.util.List;
 
 public class T148 {
     public ListNode sortList(ListNode head) {
-        Integer length = 1;
+        int length = 1;
         ListNode origin = head;
         int nodeLength = 0;
         while (origin!=null){
             origin = origin.next;
             nodeLength++;
         }
-        origin = head;
-        while (nodeLength>length){
-            while(walk(origin,length)!=null){
-                origin = merge(origin,length);
-                origin = walk(origin,length*2);
+
+        ListNode pre = new ListNode();
+        ListNode res = pre;
+        ListNode h = head;
+        res.next = head;
+        while (nodeLength > length) {
+            ListNode p1, p2;
+
+            while (h != null){
+                p1 = h;
+                int i = length;
+                for (; i > 0 && h!=null; i--) {
+                    h = h.next;
+                }
+                if (i>0) {
+                    continue;
+                }
+                p2 = h;
+                i = length;
+                for (; i > 0 && h != null ; i--) {
+                    h = h.next;
+                }
+                int l1 = length, l2 = length - i;
+                while (l1 > 0 && l2 > 0) {
+                    if (p1.val > p2.val) {
+                        l2--;
+                        res.next = p2;
+                        p2 = p2.next;
+                    } else {
+                        l1--;
+                        res.next = p1;
+                        p1 = p1.next;
+                    }
+                    res = res.next;
+                }
+                while (l1 > 0) {
+                    l1--;
+
+                    res.next = p1;
+                    p1 = p1.next;
+                    res = res.next;
+                }
+                while (l2 > 0) {
+                    l2--;
+                    res.next = p2;
+                    p2 = p2.next;
+                    res = res.next;
+                }
+                res.next = h;
             }
-            length *=2;
+            length *= 2;
+            h = pre.next;
+            res = pre;
         }
 
-        return origin;
-    }
-
-    ListNode merge (ListNode origin, Integer length){
-        ListNode result = new ListNode();
-        ListNode pNode = result;
-        int l1 = length, l2 = length;
-        ListNode p1 = origin;
-        ListNode p2 = walk(origin,length);
-        ListNode next = walk(p2,length);
-        while (l1 >0 &&l2>0){
-            if (p1.val > p2.val){
-                l2--;
-                p2 = p2.next;
-                pNode.next = p2;
-            }else {
-                l1--;
-                p1 = p1.next;
-                pNode.next = p1;
-            }
-            pNode = pNode.next;
-        }
-        while (l1 > 0){
-            l1--;
-            p1 = p1.next;
-            pNode.next = p1;
-            pNode = pNode.next;
-        }
-        while (l2 > 0){
-            l2--;
-            p2 = p2.next;
-            pNode.next = p2;
-            pNode = pNode.next;
-        }
-        pNode.next = next;
-        return result.next;
-    }
-
-    ListNode walk (ListNode origin, Integer step){
-        for (int i = 0; i < step; i++) {
-            origin = origin.next;
-            if(origin == null) return null;
-        }
-        return origin;
+        return pre.next;
     }
 
 
     public static void main(String[] args) {
 
         int[] intarray = new int[]{
-                4,2,1,3
+                1
         };
 
         Integer[] integerarray = new Integer[]{
