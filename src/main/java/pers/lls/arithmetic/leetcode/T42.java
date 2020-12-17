b90c03765f97233b569aab2ff65b966451c0b44d
lils <lils@broada.com>
2020-12-17 21:09:48 +0800

    算法题

--- src/main/java/pers/lls/arithmetic/leetcode/T42.java
+++ src/main/java/pers/lls/arithmetic/leetcode/T42.java
@@ -10,7 +10,25 @@ import java.util.List;
 
 public class T42 {
     public int trap(int[] height) {
-        return 0;
+        int leftMax = 0;
+        int rightMax = 0;
+        int length = height.length;
+        int[] leftHigh = new int[length];
+        int[] rightHigh = new int[length];
+        int result = 0;
+
+        for (int i = 0; i < length; i++) {
+            leftMax = Math.max(leftMax,height[i]);
+            leftHigh[i] = leftMax;
+        }
+        for (int i = length - 1; i >= 0; i--) {
+            rightMax = Math.max(rightMax, height[i]);
+            rightHigh[i] = rightMax;
+        }
+        for (int i = 0; i < length; i++) {
+            result += Math.min(leftHigh[i],rightHigh[i])-height[i];
+        }
+        return result;
     }
 
 
@@ -29,7 +47,7 @@ public class T42 {
 
         ListNode listNode2 = new ListNode(1,new ListNode(3,new ListNode(4)));
 
-        System.out.println(JSONObject.toJSONString(new T42().trap(new int[]{7,2,3,6,7})));
+        System.out.println(JSONObject.toJSONString(new T42().trap(new int[]{})));
     }
 }
 
