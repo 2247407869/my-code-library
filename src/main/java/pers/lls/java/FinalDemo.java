9e37758ae2e46b3c0fc6500613dac4b460e4c6d8
lils <2247407869@qq.com>
2020-12-30 11:30:44 +0800

    guava

---
+++ src/main/java/pers/lls/java/FinalDemo.java
@@ -0,0 +1,15 @@
+package pers.lls.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class FinalDemo {
+    // 被final修饰的引用类型无法再指向其他对象
+    private final List<Integer> integerList = new ArrayList<>();
+
+    public static void main(String[] args) {
+    }
+}
