cb09af25efc8803fc092462c567b0bd843878638
LLS <2247407869@qq.com>
2022-11-27 14:10:51 +0800

    jpa

--- springboot-demo/src/main/java/com/example/demo/domain/User.java
+++ springboot-demo/src/main/java/com/example/demo/domain/User.java
@@ -3,13 +3,20 @@ package com.example.demo.domain;
 import java.io.Serializable;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * user
  * @author 
  */
 @Data
+@Entity
 public class User implements Serializable {
-    private Integer id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
     private String name;
 
