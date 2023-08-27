8bfc9d9f843e01eb60d07f488340e82d96a0388a
LLS <2247407869@qq.com>
2023-08-27 22:03:40 +0800

    jpa

--- springboot-demo/src/main/java/com/example/demo/controller/TestController.java
+++ springboot-demo/src/main/java/com/example/demo/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.UserRepositoty;
 import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,10 +44,10 @@ public class TestController {
      */
     @GetMapping("/users/{id}")
     @ResponseBody
-    public User getUser(@PathVariable("id") Long id) {
+    public UserDTO getUser(@PathVariable("id") Long id) {
 //        User user = userDao.selectByPrimaryKey(id);
         User user = userRepositoty.getOne(id);
-        return user;
+        return UserDTO.formPO(user);
     }
 
     /**
